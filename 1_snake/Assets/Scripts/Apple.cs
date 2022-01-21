480e41dd10865820bbb8b50f69f8b77b31439ad5
Vinicius Teodoro <vinitbasilio@hotmail.com>
2022-01-21 18:45:57 -0300

    Apple changes place on collision

--- 1_snake/Assets/Scripts/Apple.cs
+++ 1_snake/Assets/Scripts/Apple.cs
@@ -1,8 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
- 
+   private void Start()
+   {
+      RandomizePosition();
+   }
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      if(other.tag == "Player") RandomizePosition();
+   }
+
+   public void RandomizePosition()
+   {
+      transform.position = new Vector3(Random.Range(-GameManager.cameraWidth + .3f, GameManager.cameraWidth - .3f), Random.Range(-GameManager.cameraHeight + .3f, GameManager.cameraHeight - .3f), 0);
+   }
 }
--- 1_snake/Assets/Scripts/GameManager.cs
+++ 1_snake/Assets/Scripts/GameManager.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+   public static float cameraWidth = 10.0f;
+   public static float cameraHeight = 5.1f;
 }
